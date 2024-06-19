56f0589bafc1c793a0a539ec4844f7ba54ab8c09
sannidhya <snsannidhya1512@gmail.com>
2024-06-19 12:09:52 +0530

    create sign in api routes

--- api/index.js
+++ api/index.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
+    console.error("Error connecting to MongoDB:", err.message);
   });
 
 const app = express();
@@ -27,7 +28,7 @@ app.use("/api/auth", authRouter);
 // Global error handling middleware (place at the bottom)
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
@@ -35,7 +36,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = 5454;
+const PORT = 8080;
 app.listen(PORT, () => {
-  console.log("Server is listening on port", PORT);
+  console.log(`Server is listening on port ${PORT}`);
 });
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import express from "express";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {
-  signup(req, res, next); // Pass next to signup function
+router.post("/signup", (req, res, next) => {
+  signup(req, res, next);
 });
+
+router.post("/signin", (req, res, next) => {
+  signin(req, res, next);
+});
+
 export default router;
