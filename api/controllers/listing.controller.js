09e58b2d3a2ea2b2762a5198a62df5ded7f69737
sannidhya <snsannidhya1512@gmail.com>
2024-06-25 17:21:55 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -47,3 +47,15 @@ export const updateListing = async (req, res, next) => {
     res.status(200).json(updatetdListing);
   } catch (error) {}
 };
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
